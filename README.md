f44d5145d4129849441c26d8318644003cce6d51
Aleksandar Simovic <alexander.simovic@gmail.com>
2020-06-17 16:46:50 +0200

    updating the component to nodejs12, minor refactors

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 
-## SNS -> Lambda -> DynamoDb
+# SNS -> Lambda -> DynamoDb
 
-#### Description
-This is a SNS Lambda subscriber that stores notification data into a DynamoDB table. 
+## Description
 
-Written in Node.js. 
+This is a SNS Lambda subscriber that stores notification data into a DynamoDB table.
 
-It's a Nuts & Bolts application component for AWS Serverless Application Repository.
+Written in Node.js.
+
+It's a serverless application component, instantly deployable via AWS Serverless Application Repository.
+
+## Latest Release - 2.0.0
+
+- Upgrading the Node.js release to 12.x LTS
+- Using AWS SAM CLI as a deployment tool
---
+++ src/lambda.js
@@ -0,0 +1,27 @@
+const AWS = require('aws-sdk');
+const docClient = new AWS.DynamoDB.DocumentClient();
+const { v4: uuidv4 } = require('uuid');
+const parseSnsMessage = require('./parse-sns-event');
+const TABLE_NAME = process.env.TABLE_NAME;
+const PRIMARY_KEY = process.env.PRIMARY_KEY;
+
+exports.handler = (event) => {
+  let messages = parseSnsMessage(event);
+  return Promise.all(messages.map(saveToDynamoDB));
+};
+
+saveToDynamoDB = async (data) => {
+  if (!data) return;
+  data[PRIMARY_KEY] = uuidv4();
+  const params = {
+    TableName: TABLE_NAME,
+    Item: data
+  };
+
+  try {
+    return await docClient.put(params).promise()
+  } catch (e) {
+    console.log(err);
+    throw err;
+  }
+};
